7708ec1660f632f27cf95ca3437f2407e4bd1e49
Anton Ko <anton.ko0408@gmail.com>
2025-01-25 13:51:08 -0800

    implemented the js file for map (will need to catch errors)

--- map.js
+++ map.js
@@ -1,12 +1,31 @@
+WEB_INTERFACE = "http://127.0.0.1:5000"
+
+document.addEventListener('DOMContentLoaded', () => {
+    const jsonCoords = getCoords(); // Safely call getCoords after the DOM is loaded
+    var latitude = jsonCoords.Latitude
+    var longitude = jsonCoords.Longitude
+    displayMap(latitude, longitude)  // might have problem if no return value from python flask file -> try and catch fix later
+  });
 
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+function getCoords() {
+    fetch(WEB_INTERFACE + "/locationFromIP")
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response)
+    });
+}
 
-var ourMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
+function displayMap(latitude, longitude) {
+    var map = L.map('map').setView([latitude, longitude], 13);
 
-ourMap.addTo(map);
+    var ourMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        })
+
+    ourMap.addTo(map);
+
+}
 
 /*L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
